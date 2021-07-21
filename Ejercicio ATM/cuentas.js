805cc2766a9eca9e7ca4563d96ba55f7ebe0a839
Carlos <karsyano@hotmail.com>
2021-07-20 20:51:24 -0700

    Test

--- Ejercicio ATM/cuentas.js
+++ Ejercicio ATM/cuentas.js
@@ -1,16 +1,8 @@
-var input1 = prompt("Digite su numero de cuenta");
-var input2 = prompt("Digite su numero de PIN");
-
-function aprobadocuenta() {
-  if (input1 == usuario1.cuentanum && input2 == usuario1.pin) {
-    console.log("Welcome to Maya Bank Mali");
-  } else if (input1 == 222 && input2 == 333) {
-    console.log("Welcome to Maya Bank Gera");
-  } else if (input1 == 333 && input2 == 444) {
-    console.log("Welcome to Maya Bank Maui");
-  } else {
-    ("Account does not exist");
-  }
+function message() {
+  var input1 = document.getElementById("typeEmailX").value;
+  var input2 = document.getElementById("typePasswordX").value;
+console.log(input1)
+console.log(input2)
 }
 
 class banco {
@@ -28,11 +20,20 @@ class banco {
   }
 }
 
+function aprobadocuenta() {
+  if (input1 == usuario1.cuentanum && input2 == usuario1.pin) {
+    console.log("Welcome to Maya Bank Mali");
+  } else if (input1 == 222 && input2 == 333) {
+    console.log("Welcome to Maya Bank Gera");
+  } else if (input1 == 333 && input2 == 444) {
+    console.log("Welcome to Maya Bank Maui");
+  } else {
+    ("Account does not exist");
+  }
+}
+
 const usuario1 = new banco("Mali", "2000", "helloworld", 123456789, 111);
 const usuario2 = new banco("Gera", "1540", "l33t", 987654321, 222);
 const usuario3 = new banco("Maui", "245", "123", 456789123, 333);
 
 usuario1.verinfo();
-usuario2.verinfo();
-usuario3.verinfo();
-aprobadocuenta();
